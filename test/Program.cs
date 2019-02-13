315e74e3e3b7c9a6664f62ccb87aebe180869215
visualstudio <visualstudio@122-PC02>
2019-02-13 09:18:08 -0500

    commit 1st

--- test/Program.cs
+++ test/Program.cs
@@ -9,7 +9,37 @@ namespace test
     class Program
     {
         static void Main(string[] args)
-        {
+      
+            {
+                new DogHouse().Run();
+            }
         }
     }
-}
+    class Dog
+    {   // class Dog is Abstract Data Type
+        public string DogName;
+        public int age;
+        public string favoriteToy;
+        public int milesWalkedPerDay;
+    }
+
+    class DogHouse
+    {
+        Dog D1, D2, D3, D4, Temp;
+        string[] DogNames;
+        Dog[] Dogs;
+
+        public void Run()
+        {
+            Dogs = new Dog[5];
+            DogNames = new string[5] { "Peanut", "Fifi", "Fido", "Clearence", "Bigi" };
+
+            for (int i = 0; i < Dogs.Length; i++)
+            {
+                Dogs[i] = new Dog();
+                Dogs[i].DogName = DogNames[i];
+            }
+        }
+ }
+
+
